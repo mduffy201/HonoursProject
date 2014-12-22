487ad6e7ef0f2a187e7e70d3ee8bf2b5d35eadeb
mduffy201 <mduffy201@caledonian.ac.uk>
2014-12-22 16:31:49 +0000

    Simple movement added
    Sample assets movement script adapted for use.
    - Animation.
    +Movement left and right, jumping.
    +Ground check using attached transform and overlap circle.
    +User input using input manager and CrossPlatformInputManager class from
    sample assets download.
    +Camera follow class from sample assets used.

---
+++ Unity/Assets/MyAssets/Controls2D.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+
+//Controls require Playermovement2D class attached to player object
+[RequireComponent(typeof (PlayerMovement2D))]
+
+public class Controls2D : MonoBehaviour {
+
+	private PlayerMovement2D character;
+	private bool jump;
+
+	private void Awake()
+	{
+		character = GetComponent<PlayerMovement2D>();
+	}
+	
+	private void Update()
+	{
+		if(!jump)
+			// Read the jump input in Update so button presses aren't missed.
+			jump = CrossPlatformInputManager.GetButtonDown("Jump");
+	}
+	
+	private void FixedUpdate()
+	{
+		// Read the inputs.
+		//bool crouch = Input.GetKey(KeyCode.LeftControl);
+		float h = CrossPlatformInputManager.GetAxis("Horizontal");
+		// Pass all parameters to the character control script.
+		character.Move(h, jump);
+		jump = false;
+	}
+}
