870e5fdb4877beb820beec28a9d33c04ec4f83e7
mduffy201 <mduffy201@caledonian.ac.uk>
2015-02-16 19:48:36 +0000

    Shooting added to player

---
+++ Unity/Assets/Scirpts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	private Vector3 currentPosition;
+	private Vector3 direction;
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		currentPosition = transform.position;
+		direction = new Vector3 (1.0f, 0.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Translate (new Vector3 (direction.x * speed * Time.deltaTime, 0.0f, 0.0f));
+	
+	}
+}
---
+++ Unity/Assets/Scirpts/Gun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public Transform gun;
+	private GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+		gun = transform.Find ("GunPosition");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			Debug.Log("Fire Pressed");	
+			bullet = (GameObject)Instantiate((GameObject)Resources.Load("Bullet/Bullet"), gun.position, Quaternion.identity);
+		}
+	}
+}
