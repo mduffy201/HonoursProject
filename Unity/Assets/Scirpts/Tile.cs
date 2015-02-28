421ce137135144faa477f3bf5af5db9f83c98862
mduffy201 <mduffy201@caledonian.ac.uk>
2015-02-28 14:06:51 +0000

    Level geometry improved
    Diagonal tile neighbours added.
    CA functionality for edge tiles added.
    CA rules added

--- Unity/Assets/Scirpts/Tile.cs
+++ Unity/Assets/Scirpts/Tile.cs
@@ -23,6 +23,7 @@ public class Tile
 		}
 		private Tile[] tile_neighbours;
 		public int state = 0;
+		public bool edge = false;
 		// Create a variable that will hold the tile type for each tile that is created
 		// We can then check each tile to see if we can walk on it or if something else should happen
 		public TileType tileType;
@@ -34,17 +35,34 @@ public class Tile
 		// Basic Empty constructor that sets the default type to null so that they will not be created
 		public Tile ()
 		{
+
 				tileType = TileType.Null;
-				tile_neighbours = new Tile [4];
+				tile_neighbours = new Tile [8];
 
 		}
 
-	public void SetNeighbours(Tile tile1, Tile tile2, Tile tile3, Tile tile4){
-		tile_neighbours [0] = tile1; //Down
-		tile_neighbours [1] = tile2; //Left
-		tile_neighbours [2] = tile3; //Up
-		tile_neighbours [3] = tile4; //Right
-	}
+		public void SetNeighbours (Tile tileDown, Tile tileLeft, Tile tileUp, Tile tileRight,
+	                          Tile tileDownLeft, Tile tileUpLeft, Tile tileUpRight, Tile tileDownRight)
+		{
+		/*if( tileDown == null & tileLeft == null){
+			Debug.Log ("LEFT BOTTOM CONER REACHED!!!");
+		}*/
+				tile_neighbours [0] = tileDown; //Down
+				tile_neighbours [1] = tileLeft; //Left
+				tile_neighbours [2] = tileUp; //Up
+				tile_neighbours [3] = tileRight; //Right
+				tile_neighbours [4] = tileDownLeft; //Down-Left
+				tile_neighbours [5] = tileUpLeft; //Up-Left 
+				tile_neighbours [6] = tileUpRight; //Up-Right
+				tile_neighbours [7] = tileDownRight; //Down-Right
+
+		if (tileDown == null & tileLeft == null) {
+						if (tile_neighbours [0] == null) {
+								Debug.Log ("LEFT BOTTOM CONER REACHED!!!: ");
+						}
+				}
+		}
+
 		public void SwitchState ()
 		{
 				if (state == 0) {
@@ -56,20 +74,66 @@ public class Tile
 
 		public void UpdateTile ()
 		{
-		if (tile_neighbours [0] != null) {
-						if (state == 1) {
-								int noDead = 0;
-								for (int i = 0; i < 4; i++) {
-										if (tile_neighbours [i].state == 0) {
-												noDead++;
-										}
-								}
-								if (noDead > 2) {
-										state = 0;
-								}
+				
+					RuleTwo ();
+					//RuleThree ();
+					RuleFour ();
+		}
+
+		private void RuleOne ()
+		{
+				//If tile dead and surrounded by alive on above and below rows, make alive
+				if (state == 0) {
+						if (tile_neighbours [5].state == 1 &&
+								tile_neighbours [2].state == 1 &&
+								tile_neighbours [6].state == 1 &&
+								tile_neighbours [4].state == 1 &&
+								tile_neighbours [0].state == 1 &&
+								tile_neighbours [7].state == 1) {
+								state = 1;
+						}	
 		
 		
-						}
 				}
 		}
+
+		private void RuleTwo ()
+		{
+				if (state == 0) {
+						int alive = 0;
+						for (int i = 0; i < tile_neighbours.Length; i++) {
+				if( tile_neighbours[i] != null){
+								if (tile_neighbours [i].state == 1 ) {
+										alive++;
+								}
+				}
+						}
+						if (alive >= 5) {
+								state = 1;
+						}
+		
+				}
+		}
+	private void RuleThree ()
+	{
+		if (state == 1) {
+			int alive = 0;
+			for (int i = 0; i < tile_neighbours.Length; i++) {
+				if (tile_neighbours [i].state == 1) {
+					alive++;
+				}
+			}
+			if (alive < 1) {
+				state = 0;
+			}
+			
+		}
+	}
+	private void RuleFour(){
+		if (state == 0) {
+			if(tile_neighbours[0] == null){
+				state = 1;
+			}		
+		}
+	}
 }
--- Unity/Assets/Scirpts/TileGrid.cs
+++ Unity/Assets/Scirpts/TileGrid.cs
@@ -60,7 +60,7 @@ public class TileGrid : MonoBehaviour
 				CreateLSystemString ();
 				GeneratePatternsUsingLSyem ();
 				DrawFullMap ();
-
+				LoadNeighbours ();
 
 
 
@@ -94,25 +94,119 @@ public class TileGrid : MonoBehaviour
 				return levelMap [xPos, yPos];
 		}
 
-		private void LoadNeibours ()
+		private void LoadNeighbours ()
 		{
-				int level_length = levelMap.GetLength (0);
-				int level_height = levelMap.GetLength (1);
+				int level_length = full_level.GetLength (0);
+				int level_height = full_level.GetLength (1);
 		
-				Debug.Log ("Level length: " + level_length);
-		
+				//			Debug.Log ("Level length: " + level_length);
+
+		int iLevel_length = level_length - 1;
+		int iLevel_height = level_height - 1;
+
+				//CORNER TILES
+				//bottomleft
+				full_level [0, 0].SetNeighbours (null, null, full_level [0, 1], full_level [1, 0], null, null, full_level [1, 1], null);
+				//topleft
+				full_level [0, iLevel_height].SetNeighbours (full_level [0, iLevel_height - 1], null, null, full_level [1, iLevel_height], null, null, null, full_level [1, iLevel_height - 1]);
+				//bottomright
+				full_level [iLevel_length, 0].SetNeighbours (null, full_level [iLevel_length - 1, 0], full_level [iLevel_length, 1], null, null, full_level [iLevel_length - 1, 1], null, null);
+				//topright
+				full_level [0, iLevel_height].SetNeighbours (full_level [iLevel_length, iLevel_height - 1], full_level [iLevel_length - 1, iLevel_height], null, null, full_level [iLevel_length - 1, iLevel_height - 1], null, null, null);
+
+				
+
+
+
 				//Go up each column, left to right
-				for (int i =1; i < level_length-1; i++) {
-						for (int j =1; j < level_height-1; j++) {
-								
-								levelMap [i, j].SetNeighbours (
-					levelMap [i, j - 1],
-					levelMap [i - 1, j],
-					levelMap [i, j + 1],
-					levelMap [i + 1, j]
-								);
+				for (int i =0; i < level_length; i++) {
+						for (int j =0; j < level_height; j++) {
+
+								//LEFT EDGE TILES			
+								if (i == 0 && j != 0 && j != iLevel_height) {
+									//Set non-edge tiles
+									full_level [i, j].SetNeighbours (
+										//Straight 
+										full_level [i, j - 1], //0
+										null,
+										full_level [i, j + 1], //2
+										full_level [i + 1, j], //3
+										//Diagonals
+										null,
+										null,
+										full_level [i + 1, j + 1], //6
+										full_level [i + 1, j - 1] //7
+										);
+								} 
+								//Right EDGE TILES
+								else if (i == iLevel_length && j != 0 && j != iLevel_height) {
+									full_level [i, j].SetNeighbours (
+										//Straight 
+										full_level [i, j - 1], //0
+										full_level [i - 1, j], //1
+										full_level [i, j + 1], //2
+										null,
+										//Diagonals
+										full_level [i - 1, j - 1], //4
+										full_level [i - 1, j + 1], //5
+										null,
+										null
+										);
+								}
+								//TOP EDGE TILES
+								else if (j == iLevel_height && i != 0 && i != iLevel_length) {
+									full_level [i, j].SetNeighbours (
+										//Straight 
+										full_level [i, j - 1], //0
+										full_level [i - 1, j], //1
+										null,
+										full_level [i + 1, j], //3
+										//Diagonals
+										full_level [i - 1, j - 1],//4
+										null,
+										null,
+										full_level [i + 1, j - 1]//7
+										);
+
+								}
+								//BOTTOM EDGE TILE
+								else if (j == 0 && i != 0 && i != iLevel_length) {
+									//Set non-edge tiles
+									full_level [i, j].SetNeighbours (
+										//Straight 
+										null,
+										full_level [i - 1, j], //1
+										full_level [i, j + 1], //2
+										full_level [i + 1, j],//3
+										//Diagonals
+										null,
+										full_level [i - 1, j + 1], //5
+										full_level [i + 1, j + 1], //6
+										null
+										);
 								
 
+				
+				}else {
+					//Eliminate corners
+					if(i != 0 && i != iLevel_length && j != 0 && j != iLevel_height){
+										//Set non-edge tiles
+										full_level [i, j].SetNeighbours (
+											//Straight 
+											full_level [i, j - 1],
+											full_level [i - 1, j],
+											full_level [i, j + 1],
+											full_level [i + 1, j],
+											//Diagonals
+											full_level [i - 1, j - 1],
+											full_level [i - 1, j + 1],
+											full_level [i + 1, j + 1],
+											full_level [i + 1, j - 1]
+										);
+					}
+
+								}
+			
 
 						}
 				}
@@ -120,8 +214,8 @@ public class TileGrid : MonoBehaviour
 
 		public void UpdateTileMap ()
 		{
-				int level_length = levelMap.GetLength (0);
-				int level_height = levelMap.GetLength (1);
+				int level_length = full_level.GetLength (0);
+				int level_height = full_level.GetLength (1);
 		
 
 		
@@ -129,13 +223,13 @@ public class TileGrid : MonoBehaviour
 				for (int i =0; i < level_length; i++) {
 						for (int j =0; j < level_height; j++) {
 								//Update each tile
-								levelMap [i, j].UpdateTile ();
+								full_level [i, j].UpdateTile ();
 						}
 				}		
 				//re load neibours
-				LoadNeibours ();
+				LoadNeighbours ();
 				//Re draw
-				//DrawTileMap ();
+				DrawFullMap ();
 		}
 
 		
@@ -184,7 +278,7 @@ public class TileGrid : MonoBehaviour
 						}
 						temp = result;
 
-						Debug.Log ("L-System: " + result);
+						//Debug.Log ("L-System: " + result);
 						lSystem = result;
 				}
 		}
@@ -204,10 +298,10 @@ public class TileGrid : MonoBehaviour
 
 								break;
 						case 'B':
-								GenAPattern (i + 1);
+								GenBPattern (i + 1);
 								break;
 						case 'C':
-								GenAPattern (i + 1);
+								GenCPattern (i + 1);
 								break;
 
 						}
@@ -216,32 +310,67 @@ public class TileGrid : MonoBehaviour
 
 		private void GenAPattern (int x_placement)
 		{
-
-
-				//	pattern = new Tile[pattern_horizontal, pattern_vertical];
-
 				for (int i = 0; i < pattern_horizontal; i++) {
-						// For each row, cycle through an entire column
-						//for (int j = 0; j < 2; j++) {
-						// Now we combine the loops' indexes to make sure each slot has a null tile
-						int j = 1;
-						Random.seed = 100;
-						int tile_state = (int)(Random.value) % 2;
-		//	Debug.Log ("Pattern: " + x_placement);	
-			int place = (x_placement-1) * pattern_horizontal + i;
-			Debug.Log ("Level place: " + place);
-			//Debug.Log ("X: " + i + " RANDOM VALUE: " + Random.value);
-						//Debug.Log ("tile state: " + tile_state);
-						if (tile_state == 0) {
-		//						Debug.Log ("switched: " + Random.value * 100);
-								full_level [place , j].SwitchState ();
+						int place = (x_placement - 1) * pattern_horizontal + i;
+						int j = 0;
+						Random.seed = (int)place;
+						int random_value = (int)(Random.value * 10) * i;
+						int tile_state = random_value % 10;
+				
+						//int place = (x_placement - 1) * pattern_horizontal + i;
+					
+						//Debug.Log ("Random value: " + random_value);			
+						if (tile_state == 0 || tile_state == 1) {
+								//						Debug.Log ("switched: " + Random.value * 100);
+								full_level [place, j].SwitchState ();
 						}
-
-
-						//}
 				}
+		}
 
+		private void GenBPattern (int x_placement)
+		{
+				for (int i = 0; i < pattern_horizontal; i++) {
+						for (int j = 0; j < pattern_vertical; j++) {
+								
+								if (j == 0 || j == 2) {
+										int place = (x_placement - 1) * pattern_horizontal + i + j;
 
+										Random.seed = (int)place;
+										int random_value = (int)(Random.value * 10) * i;
+										int tile_state = random_value % 4;
+			
+										
+										//Debug.Log ("Random value: " + random_value);			
+										if (tile_state == 0 || tile_state == 1) {
+												//						Debug.Log ("switched: " + Random.value * 100);
+												full_level [place, j].SwitchState ();
+										}
+								}
+						}
+				}
+		}
+
+		private void GenCPattern (int x_placement)
+		{
+				for (int i = 0; i < pattern_horizontal; i++) {
+						for (int j = 0; j < pattern_vertical; j++) {
+								if (j == 0 || j == 2 || j == 4) {
+										int place = (x_placement - 1) * pattern_horizontal + i + j;
+								
+										Random.seed = (int)place;
+										int random_value = (int)(Random.value * 10) * i;
+										int tile_state = random_value % 4;
+			
+						
+			
+										//Debug.Log ("Random value: " + random_value);			
+										if (tile_state == 0 || tile_state == 1) {
+												//						Debug.Log ("switched: " + Random.value * 100);
+												full_level [place, j].SwitchState ();
+										}
+								}
+						}
+				}
 		}
 
 		private void DrawFullMap ()
@@ -280,114 +409,3 @@ public class TileGrid : MonoBehaviour
 		}
 		
 }
-// Generate Level
-/*public void LoadMap1()
-	{
-		// Set the name of the tileset that this map will use (it has to match the name of the folder within Resources for each tileset)
-		//setName = "Main Map";
-		// Using add tile, send the X and Y position as well as the type of tile
-		//DateTime time =  new DateTime();
-		System.DateTime time = System.DateTime.Now;
-
-		Random.seed = time.Millisecond;
-		int startpos = (int)(Random.value* 10000) %(int)worldSize.y;
-		int endpos = (int)(Random.value* 10000) %(int)worldSize.y;
-		Debug.Log("Start pos:  " + (Random.value* 1000));
-
-		for (int i = 0; i < worldSize.x; i++)
-		{
-			for (int j = 0; j < worldSize.y; j++)
-			{
-				//random assignment
-				 
-				Random.seed = i * j-1;
-				int tile_state = (int)(Random.value* 1000) %2;
-				//Debug.Log("Random value: " + tt);
-
-				if(tile_state==0){
-					//AddTile (i,j,Tile.TileType.Sky);
-				}
-				if(tile_state==1){
-					//AddTile (i,j,Tile.TileType.Platform);
-				}
-				if (j == startpos && i ==0){
-					//AddTile (i,j,Tile.TileType.Start);
-				}
-				if (j == endpos && i == worldSize.x-1){
-					//AddTile (i,j,Tile.TileType.Finish);
-				}
-			}
-		}
-	}*/
-// AddTile is pretty straight forward, really
-/*public void AddTile(int xPos, int yPos, Tile.TileType type)
-	{
-		// Set the tile type based on the xPos and yPos (explained a bit more wit the next comment)
-		levelMap[xPos,yPos].tileType = type;
-		
-		// I'm using xPos and yPos for the array's index as well as the actual position of the tile
-		// This makes the tile map extremely simple since we now know worldMap[5,17] is at position 5x,17y.
-		levelMap[xPos,yPos].tilePos = new Vector2(xPos,yPos);
-	}*/
-// Quick and dirty way to load data for the map to generate. The map is loaded based on AddTile, check the LoadBonusMap method
-// for details on how I'm creating the simple tile map
-//LoadBonusMap();
-//LoadMap1 ();
-/*	public void initTileMap ()
-		{
-				//initial tile map for level, empty tiles.
-				//levelMap;
-				int level_length = levelMap.GetLength (0);
-				int level_height = levelMap.GetLength (1);
-
-				Debug.Log ("Level length: " + level_length);
-
-				//Go up each column, left to right
-				for (int i =0; i < level_length; i++) {
-						for (int j =0; j < 2; j++) {
-
-								Random.seed = i * j - 1;
-								int tile_state = (int)(Random.value * 1000) % 2;
-
-								if (tile_state == 0) {
-										levelMap [i, j].SwitchState ();
-								}
-			
-						}		
-				}
-		}*/
-
-// The method that creates a sprite for each tile represented in worldMap[,]
-/*	public void DrawTileMap ()
-		{
-				// Loop structure is the same as the loops above ^^^
-				for (int i = 0; i < worldSize.x; i++) {
-						for (int j = 0; j < worldSize.y; j++) {
-
-								if (levelMap [i, j].state == 0) {
-										levelMap [i, j].tileType = Tile.TileType.Sky;
-
-								} else if (levelMap [i, j].state == 1) {
-										levelMap [i, j].tileType = Tile.TileType.Platform;
-								}
-
-
-
-								// Check to see if the tile type is null or not
-								// If the tile is null, we know not to try to draw a sprite for it
-								if (levelMap [i, j].tileType != Tile.TileType.Null) {
-										// If the tile isn't null, it must exist so let's do some stuff
-										// We're going through the Resources folder, within the Main Map directory and finding a sprite that matches
-										// the TYPE of tile that we've found. ***Make sure the name of the sprite matches the tile types***
-										GameObject tileObject;
-										// We're creating an instance of the sprite object that we find based on the tiletype compared to the object name, storing it in tileSprite
-										tileObject = (GameObject)Instantiate (Resources.Load ("Tiles/Tile"), levelMap [i, j].tilePos, Quaternion.identity);
-										tileObject.GetComponent<TileObject> ().SetTile (levelMap [i, j]);
-										//bullet.GetComponent<Bullet>().SetDir(newdir)
-										// Now set the newly created sprite's parent to be the map parent that we created earlier 
-										tileObject.transform.parent = tileParent.transform;
-								}
-						}
-				}
-		}*/
-
